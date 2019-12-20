c6dc83874894c34f50bb70f38857a41f200ce5ab
hsu <janice_95@126.com>
2019-12-20 21:29:55 +0800

    ”主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>主页</div>
+<el-container>
+  <el-aside width="200px">Aside</el-aside>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
