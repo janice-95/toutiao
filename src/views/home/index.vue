4e0a455b095bd6230ced1d551711bca3e521cf96
hsu <janice_95@126.com>
2019-12-20 23:28:00 +0800

    主页模块-左侧导航菜单

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,19 +1,26 @@
 <template>
-<el-container>
-  <el-aside width="200px">Aside</el-aside>
+  <!-- 大容器 -->
   <el-container>
-    <el-header>Header</el-header>
-    <el-main>Main</el-main>
+    <!-- 左面 -->
+    <el-aside style="height:100vh;background-color:#353b4e;width:200px">
+      <layout-aside></layout-aside>
+    </el-aside>
+    <el-container>
+      <!-- 右面上下 -->
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
   </el-container>
-</el-container>
 </template>
 
 <script>
+import layoutAsid from '../../components/home/layout-aside'
 export default {
-
+  components: {
+    'layout-aside': layoutAsid
+  }
 }
 </script>
 
 <style>
-
 </style>
