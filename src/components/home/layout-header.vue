8ae72eada52216b1c2c09ac8f4eda4f3f328a955
hsu <janice_95@126.com>
2019-12-21 01:44:35 +0800

    主页模块-头部退出系统

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -14,13 +14,13 @@
             <el-button style="border:0;background:#fff;">消息</el-button>
           </el-tooltip>
           <img :src="!userInfo.photo ? userInfo.photo : defaultImg " alt="" style="width:35px;height:35px;border-radius=50%;vertical-align: middle;">
-           <el-dropdown>
+           <el-dropdown @command="clickMenu">
                  <!-- 匿名插槽  下拉菜单显示的元素内容 -->
                  <span>{{userInfo.name}}</span>
                  <el-dropdown-menu slot="dropdown">
-                     <el-dropdown-item>个人信息</el-dropdown-item>
-                     <el-dropdown-item>git地址</el-dropdown-item>
-                     <el-dropdown-item>退出</el-dropdown-item>
+                     <el-dropdown-item command="pim">个人信息</el-dropdown-item>
+                     <el-dropdown-item command="git">git地址</el-dropdown-item>
+                     <el-dropdown-item command="lgout">退出</el-dropdown-item>
                  </el-dropdown-menu>
              </el-dropdown>
         </div>
@@ -48,6 +48,21 @@ export default {
     }).then(result => {
       this.userInfo = result.data.data
     })
+  },
+  methods: {
+    //   点击菜单项时触发
+    clickMenu (command) {
+      if (command === 'pim') {
+
+      } else if (command === 'git') {
+        //   跳转到git地址
+        window.location.href = 'https://github.com/janice-95/toutiao'
+      } else {
+        //    退出
+        window.localStorage.removeItem('user-token') // 删除令牌
+        this.$router.push('/login') // 回到登录页
+      }
+    }
   }
 }
 </script>
