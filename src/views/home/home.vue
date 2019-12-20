9c523c703260bfe233df927edaa4fe8abdd19183
hsu <janice_95@126.com>
2019-12-21 02:08:37 +0800

    主页模块-默认导航

---
+++ src/views/home/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div><img src="../../assets/img/index_bcg.jpg" alt=""></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+img{
+    height: 100vh;
+    width: 100%;
+}
+</style>
