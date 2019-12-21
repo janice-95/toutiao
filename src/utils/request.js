94cc615e6e0fb34de1f0d729bd2c2045d926a8dd
hsu <janice_95@126.com>
2019-12-21 23:37:21 +0800

    axios拦截器-统一处理异常数据

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,12 @@
 // 封装一个axios
 import axios from 'axios'
+import router from '../router'
+import { Message } from 'element-ui'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 //
 axios.interceptors.request.use((config) => {
-// 执行请求ok
-// config
+  // 执行请求ok
+  // config
   let token = window.localStorage.getItem('user-token')
   config.headers.Authorization = `Bearer ${token}`
   return config// 表示会用改请求
@@ -14,7 +16,30 @@ axios.interceptors.request.use((config) => {
 axios.interceptors.response.use(function (response) {
   // 对响应的数据处理成功时进入
   return response.data ? response.data : {}
-}, function () {
-
+}, function (error) {
+  // 对响应的数据处理错误的时进入
+  let status = error.response.status
+  let message = '未知错误'
+  switch (status) {
+    case 400:
+      message = '请求参数错误'
+      break
+    case 403:
+      message = '重新登录'
+      break
+    case 507:
+      message = 'token过期'
+      // 过期要清空缓存
+      window.localStorage.clear()
+      router.push('/login')
+      break
+    case 404:
+      message = '手机号不正确'
+      break
+    default:
+      break
+  }
+  Message({ message })
+  return new Promise(function () {})
 })
 export default axios
