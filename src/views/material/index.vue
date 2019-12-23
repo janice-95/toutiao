6f59a178acb429809edcc0170c79a32d38c56908
hsu <janice_95@126.com>
2019-12-24 00:39:09 +0800

    素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,17 +1,19 @@
 <template>
   <el-card>
-      <bread-crumb slot="header">
-        <template slot="title">素材管理</template>
-      </bread-crumb>
+    <bread-crumb slot="header">
+      <template slot="title">素材管理</template>
+    </bread-crumb>
+    <el-tabs>
+      <!-- 标签 -->
+      <el-tab-pane label="全部图片" name="all">全部</el-tab-pane>
+      <el-tab-pane label="收藏图片" name="collect">收藏</el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
