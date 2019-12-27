61788f5188cdca29f7ad961a38b814bd4836ea07
hsu <janice_95@126.com>
2019-12-27 20:24:39 +0800

    内容列表-请求数据

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -18,8 +18,7 @@
 
       <el-form-item label="频道列表:">
         <el-select v-model="searchForm.channel_id" placeholder="请选择">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
+          <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
 
@@ -41,13 +40,13 @@
               共找到10000条符合条件的内容
           </span>
       </el-row>
-      <div class='article-item'>
+      <div class='article-item' v-for="item in list" :key="item.id.toString()">
           <!-- 左侧 -->
           <div class='left'>
               <img src="../../assets/img/index_bcg.jpg" alt="">
               <div class='info'>
-                  <span>文章标题</span>
-                  <el-tag class='tag'>标签一</el-tag>
+                  <span>{{item.title}}</span>
+                  <el-tag :type="item.status | filterType" class='tag'>{{item.status|filterStatus}}</el-tag>
                   <span class='date'>2019-12-24 15:07:01</span>
               </div>
           </div>
@@ -64,29 +63,108 @@
 export default {
   data () {
     return {
+      list: [],
+      channels: [],
       searchForm: {
-        status: '',
-        name: '',
-        channel_id: ''
+        status: 5,
+        channel_id: null,
+        dateRange: []
       }
     }
+  },
+  // 过滤器
+  filters: {
+    filterStatus (value) {
+      switch (value) {
+        case 0:
+          return '草稿'
+        case 1:
+          return '待审核'
+        case 2:
+          return '已发表'
+        case 3:
+          return '审核失败'
+        default:
+          break
+      }
+    },
+    filterType (value) {
+      switch (value) {
+        case 0:
+          return 'warning'
+        case 1:
+          return 'info'
+        case 2:
+          return 'danger'
+        default:
+          break
+      }
+    }
+  },
+  methods: {
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels
+      })
+    },
+    getArticles () {
+      this.$axios({
+        url: '/articles'
+      }).then(result => {
+        this.list = result.data.results
+      })
+    }
+  },
+  created () {
+    this.getArticles()
+    this.getChannels()
   }
 }
 </script>
 
 <style lang="less" scoped>
-.article-item{
-  overflow: hidden;
-  padding: 15px 5px;
-  margin: 0 10px;
-  border-bottom: 1px solid #f2f3f5;
-  img{
-    float: left;
-    width: 150px;
-    height: 100px;
-    .info {
-      display: flex;
-    }
+ .articles {
+      .total {
+          height: 60px;
+          border-bottom: 1px dashed #ccc;
+      }
+      .article-item {
+          display: flex;
+          justify-content: space-between;
+          padding: 20px 0;
+          border-bottom: 1px solid #f2f3f5;
+          .left {
+              display: flex;
+              img {
+                  width: 180px;
+                  height: 100px;
+                  border-radius: 4px;
+              }
+              .info {
+                  height: 100px;
+                  margin-left: 10px;
+                  display: flex;
+                  flex-direction: column;
+                  justify-content: space-around;
+                  .date {
+                      color: #999;
+                      font-size:12px;
+                  }
+                  .tag {
+                      text-align: center;
+                      width:60px;
+                  }
+              }
+          }
+          .right {
+              span {
+                  font-size:14px;
+                  margin-right: 8px;
+                  cursor: pointer;
+              }
+          }
+      }
   }
-}
 </style>
